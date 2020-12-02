0da24508e07b406a08aca94d82ccc4b99fa857f5
Katelyn Gao <katelyng3@gmail.com>
2020-12-01 19:03:15 -0800

    Updated RL plotting

--- experiments/benchmark/summary.py
+++ experiments/benchmark/summary.py
@@ -7,6 +7,7 @@ import joblib
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from scipy.special import betainc
 
 DIR = os.getcwd()+'/data'
 OUTPUT = os.getcwd()+'/results'
@@ -112,9 +113,62 @@ for a in range(len(algos)):
                 varest += np.mean(reward_stderrs[a,b,:,d,f]**2)
                 avg_reward_stderrs[a,b,d,f] = np.sqrt(varest/len(seeds))
 
-# TODO: Add code used to construct paper figures
+# Construct contour plots
 
-# Construct plots
+print("Contour plots of whether MAML is better than DRS, as functions of the checkpoint and update")
+
+def welch_test(target_mean, target_std, target_n, comp_mean, comp_std, comp_n):
+    # Computes a Welch test to see if the comparitor sample is larger than the target
+    # sample. This is a one-sided test.
+
+    nu = ((target_std**2/target_n + comp_std**2/comp_n)**2/
+            (target_std**4/target_n/(target_n-1)+ comp_std**4/comp_n/(comp_n-1)))
+    t_stat = ((target_mean-comp_mean)
+            /np.sqrt(target_std**2/target_n+comp_std**2/comp_n))
+
+    return 0.5*betainc(nu/2,1/2,nu/(t_stat**2+nu))
+
+def get_tradeoff_map(task_avg_reward_means, task_avg_reward_stderrs):
+
+    num_checkpoints = task_avg_reward_means.shape[2]
+    num_updates = task_avg_reward_means.shape[3]
+    num_seeds = 5 # From above
+    bin_table = np.zeros((num_checkpoints, num_updates))
+    fin_table = np.zeros((num_checkpoints, num_updates))
+
+    for c in range(num_checkpoints):
+        for u in range(num_updates):
+            fin_table[c,u] = welch_test(task_avg_reward_means[1,c,u], np.sqrt(num_seeds)*task_avg_reward_stderrs[1,c,u], num_seeds, task_avg_reward_means[0,c,u], np.sqrt(num_seeds)*task_avg_reward_stderrs[0,c,u], num_seeds)
+
+            if avg_reward_means[0,task,c,u] > avg_reward_means[1,task,c,u]:
+                bin_table[c,u] = 1
+            else:
+                bin_table[c,u] = -1
+    return bin_table*(1-2*fin_table), bin_table
+
+yint = range(0,21,2)
+for b in range(len(environment_mode_pairs)):
+    mm, bt = get_tradeoff_map(avg_reward_means[0:2,b,:,:], avg_reward_stderrs[0:2,b,:,:])
+    plt.figure(figsize=(5,2.5))
+    plt.contourf(np.array(checkpoints).astype(int), np.array(updates).astype(int),  mm.T, np.linspace(-1.01, 1.01, 50),vmin=-1,vmax=1, cmap='RdBu_r')
+    plt.xticks(yint)
+    plt.xlabel('Meta-Training Budget')
+    plt.ylabel('Test Updates')
+    plt.tight_layout()
+    plt.savefig('ppo_promp_{}.pdf'.format('-'.join(environment_mode_pairs[b])))
+    plt.close(fig)
+
+    mm, bt = get_tradeoff_map(avg_reward_means[2:4,b,:,:], avg_reward_stderrs[2:4,b,:,:])
+    plt.figure(figsize=(5,2.5))
+    plt.contourf(np.array(checkpoints).astype(int), np.array(updates).astype(int),  mm.T, np.linspace(-1.01, 1.01, 50),vmin=-1,vmax=1, cmap='RdBu_r')
+    plt.xticks(yint)
+    plt.xlabel('Meta-Training Budget')
+    plt.ylabel('Test Updates')
+    plt.tight_layout()
+    plt.savefig('trpo_trpomaml_{}.pdf'.format('-'.join(environment_mode_pairs[b])))
+    plt.close(fig)
+
+# Construct reward plots
 
 print("Plotting rewards as function of checkpoint, after one update")
 for b in range(len(environment_mode_pairs)):
@@ -131,8 +185,9 @@ for b in range(len(environment_mode_pairs)):
     plt.plot(gap*np.asarray(checkpoints), avg_reward_means[1,b,:,1], 'b') # ProMP
     plt.fill_between(gap*np.asarray(checkpoints), avg_reward_means[1,b,:,1]-1.96*avg_reward_stderrs[1,b,:,1], avg_reward_means[1,b,:,1]+1.96*avg_reward_stderrs[1,b,:,1], facecolor='b', alpha=0.25)
     plt.xlabel('Training Timesteps')
-    plt.ylabel('Test Redward')
+    plt.ylabel('Test Reward')
     plt.savefig(os.path.join(OUTPUT, 'ppo_promp_{}_{}_reward_update_1.png'.format(environment, mode)))
+    plt.close(fig)
 
     fig = plt.figure()
     plt.plot(gap*np.asarray(checkpoints), avg_reward_means[2,b,:,1], 'r') # DRS+TRPO
@@ -140,6 +195,7 @@ for b in range(len(environment_mode_pairs)):
     plt.plot(gap*np.asarray(checkpoints), avg_reward_means[3,b,:,1], 'b') # ProMP
     plt.fill_between(gap*np.asarray(checkpoints), avg_reward_means[3,b,:,1]-1.96*avg_reward_stderrs[3,b,:,1], avg_reward_means[3,b,:,1]+1.96*avg_reward_stderrs[3,b,:,1], facecolor='b', alpha=0.25)
     plt.xlabel('Training Timesteps')
-    plt.ylabel('Test Redward')
+    plt.ylabel('Test Reward')
     plt.savefig(os.path.join(OUTPUT, 'trpo_trpomaml_{}_{}_reward_update_1.png'.format(environment, mode)))
+    plt.close(fig)
 
